a52c0497aab8552d5b5ad5d295a5152431f0f3cf
Etienne Posthumus <eposthumus@gmail.com>
2019-07-01 13:14:25 +0200

    Add Python 3 classifier and bump versione number

--- code/setup.py
+++ code/setup.py
@@ -1,10 +1,14 @@
 from distutils.core import setup
 
 setup(name="ICONCLASS",
-      version="0.3",
+      version="0.5",
       description="A multilingual subject classification system for cultural content",
       author="Etienne Posthumus",
       author_email="ep@epoz.org",
       url="https://github.com/epoz/iconclass",
-      py_modules=["iconclass", ]
+      py_modules=["iconclass", ],
+      classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+      ],
       )
