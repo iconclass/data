32ba72f76fa68c34545f94b0fc767575e0066baa
Etienne Posthumus <eposthumus@gmail.com>
2019-09-30 11:11:53 +0200

    Bump version number before upload to cheeseshop

--- code/setup.py
+++ code/setup.py
@@ -1,14 +1,15 @@
 from distutils.core import setup
 
-setup(name="ICONCLASS",
-      version="0.5",
-      description="A multilingual subject classification system for cultural content",
-      author="Etienne Posthumus",
-      author_email="ep@epoz.org",
-      url="https://github.com/epoz/iconclass",
-      py_modules=["iconclass", ],
-      classifiers=[
+setup(
+    name="ICONCLASS",
+    version="0.7",
+    description="A multilingual subject classification system for cultural content",
+    author="Etienne Posthumus",
+    author_email="ep@epoz.org",
+    url="https://github.com/epoz/iconclass",
+    py_modules=["iconclass"],
+    classifiers=[
         "Programming Language :: Python :: 3",
         "Operating System :: OS Independent",
-      ],
-      )
+    ],
+)
