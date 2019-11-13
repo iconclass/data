8ffe968a7b31b9f4d16879c63be9f688ee1517ce
Etienne Posthumus <eposthumus@gmail.com>
2019-11-13 13:27:49 +0100

    Bump version numbers

--- code/setup.py
+++ code/setup.py
@@ -1,10 +1,12 @@
 from distutils.core import setup
 
-setup(name="ICONCLASS",
-      version="0.5",
-      description="A multilingual subject classification system for cultural content",
-      author="Etienne Posthumus",
-      author_email="ep@epoz.org",
-      url="https://github.com/epoz/iconclass",
-      py_modules = ["iconclass",]
-      )
+setup(
+    name="ICONCLASS",
+    version="0.9",
+    description="A multilingual subject classification system for cultural content",
+    author="Etienne Posthumus",
+    author_email="ep@epoz.org",
+    url="https://github.com/epoz/iconclass",
+    py_modules=["iconclass"],
+)
+
