293c1d6da23222f50be0317ea83b45ad7bcd621f
Etienne Posthumus <eposthumus@gmail.com>
2019-11-25 10:54:42 +0100

    Version number 0.10 on PyPi - download DB from iconclass.org

--- code/setup.py
+++ code/setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name="ICONCLASS",
-    version="0.9",
+    version="0.10",
     description="A multilingual subject classification system for cultural content",
     author="Etienne Posthumus",
     author_email="ep@epoz.org",
